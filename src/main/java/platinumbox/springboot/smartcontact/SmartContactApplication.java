3833eea9332a284e92ee8d08db6cc2e5d2da96e7
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2023-02-09 10:47:48 +0700

    Init Project.

---
+++ src/main/java/platinumbox/springboot/smartcontact/SmartContactApplication.java
@@ -0,0 +1,13 @@
+package platinumbox.springboot.smartcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartContactApplication.class, args);
+    }
+
+}
